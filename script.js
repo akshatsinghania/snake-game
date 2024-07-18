61bc5192fbfed6e50a1f0b8aa28d80253777d5dd
akshatsinghania <singhaniaakshat1@gmail.com>
2024-07-18 21:53:50 +0530

    added one more function to change location of food randomly

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const playBoard = document.querySelector(".play-board");
 
-let foodX = 13,
-  foodY = 10;
-
+let foodX, foodY;
+const changeFoodPosition = () => {
+  //passing a random 0-30 value as food position
+  foodX = Math.floor(Math.random() * 30) + 1;
+  foodY = Math.floor(Math.random() * 30) + 1;
+};
 const initGame = () => {
   let htmlMarkup = `<div class="food" style= "grid-area:${foodY} / ${foodX}"></div>`;
   playBoard.innerHTML = htmlMarkup;
 };
+changeFoodPosition();
 initGame();
