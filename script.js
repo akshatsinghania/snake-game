a87d608fd9f929371767adcbab0ac2c5cd0e7259
akshatsinghania <singhaniaakshat1@gmail.com>
2024-07-19 14:13:57 +0530

    partially coded to move the head of snake to move with the help of keys

--- script.js
+++ script.js
@@ -3,18 +3,38 @@ const playBoard = document.querySelector(".play-board");
 let foodX, foodY;
 let snakeX = 5,
   snakeY = 10;
+let velocityX = 0,
+  velocityY = 0;
 const changeFoodPosition = () => {
   //passing a random 0-30 value as food position
   foodX = Math.floor(Math.random() * 30) + 1;
   foodY = Math.floor(Math.random() * 30) + 1;
 };
 
-const changeDirection = () => {
-  console.log(e);
+const changeDirection = (e) => {
+  //change velocity value on key press
+  if (e.key === "ArrowUp") {
+    velocityX = 0;
+    velocityY = -1;
+  } else if (e.key === "ArrowDown") {
+    velocityX = 0;
+    velocityY = 1;
+  } else if (e.key === "ArrowLeft") {
+    velocityX = -1;
+    velocityY = 0;
+  } else if (e.key === "ArrowRight") {
+    velocityX = 1;
+    velocityY = 0;
+  }
+  initGame();
 };
 
 const initGame = () => {
   let htmlMarkup = `<div class="food" style= "grid-area: ${foodY} / ${foodX}"></div>`;
+  //updating the snake head position based on the current velocity
+  snakeX += velocityX;
+  snakeY += velocityY;
+
   htmlMarkup += `<div class="head" style= "grid-area: ${snakeY} / ${snakeX}"></div>`;
   playBoard.innerHTML = htmlMarkup;
 };
