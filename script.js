8e0191c10b4059e3abe85f2e9f786c729f5d72e1
akshatsinghania <singhaniaakshat1@gmail.com>
2024-07-19 18:40:11 +0530

    further added function to make the game playable and giv parametres to the game

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const playBoard = document.querySelector(".play-board");
-
+let gameOver = false;
 let foodX, foodY;
 let snakeX = 5,
   snakeY = 10;
@@ -10,6 +10,10 @@ const changeFoodPosition = () => {
   //passing a random 0-30 value as food position
   foodX = Math.floor(Math.random() * 30) + 1;
   foodY = Math.floor(Math.random() * 30) + 1;
+};
+
+const handleGameOver = () => {
+  alert("Game Over! Press OK to replay...");
 };
 
 const changeDirection = (e) => {
@@ -30,17 +34,26 @@ const changeDirection = (e) => {
 };
 
 const initGame = () => {
+  if (gameOver) return handleGameOver();
   let htmlMarkup = `<div class="food" style= "grid-area: ${foodY} / ${foodX}"></div>`;
 
   if (snakeX === foodX && snakeY === foodY) {
     changeFoodPosition();
     snakeBody.push([foodX, foodY]); //pushing food position to snake body array
-    console.log(snakeBody);
+  }
+  for (let i = snakeBody.length - 1; i > 0; i--) {
+    //shifting forward the values of the elements in the snake body by one
+    snakeBody[i] = snakeBody[i - 1];
   }
   snakeBody[0] = [snakeX, snakeY]; //setting first element of the snake body to current snake position
-  //updating the snake head position based on the current velocity
+  //updating the snakes head position based on the current velocity
   snakeX += velocityX;
   snakeY += velocityY;
+
+  //checking if the snakes head is out of the wall,if so setting gameOver to true
+  if (snakeX <= 0 || snakeX > 30 || snakeX <= 0 || snakeY > 30) {
+    gameOver = true;
+  }
 
   for (let i = 0; i < snakeBody.length; i++) {
     //adding a div for each part of snake body
