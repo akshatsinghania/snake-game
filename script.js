c896ac25ce79dcda0276fbc3de5902dc7ed60c22
akshatsinghania <singhaniaakshat1@gmail.com>
2024-07-19 20:45:06 +0530

    added functionality to score function

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const playBoard = document.querySelector(".play-board");
+const scoreElement = document.querySelector(".score");
 let gameOver = false;
 let foodX, foodY;
 let snakeX = 5,
@@ -7,6 +8,7 @@ let snakeBody = [];
 let velocityX = 0,
   velocityY = 0;
 let setIntervalId;
+let score = 0;
 
 const changeFoodPosition = () => {
   //passing a random 0-30 value as food position
@@ -23,16 +25,16 @@ const handleGameOver = () => {
 
 const changeDirection = (e) => {
   //change velocity value on key press
-  if (e.key === "ArrowUp") {
+  if (e.key === "ArrowUp" && velocityY != 1) {
     velocityX = 0;
     velocityY = -1;
-  } else if (e.key === "ArrowDown") {
+  } else if (e.key === "ArrowDown" && velocityY != -1) {
     velocityX = 0;
     velocityY = 1;
-  } else if (e.key === "ArrowLeft") {
+  } else if (e.key === "ArrowLeft" && velocityX != 1) {
     velocityX = -1;
     velocityY = 0;
-  } else if (e.key === "ArrowRight") {
+  } else if (e.key === "ArrowRight" && velocityX != -1) {
     velocityX = 1;
     velocityY = 0;
   }
@@ -45,6 +47,9 @@ const initGame = () => {
   if (snakeX === foodX && snakeY === foodY) {
     changeFoodPosition();
     snakeBody.push([foodX, foodY]); //pushing food position to snake body array
+    score++; //increment score by 1
+
+    scoreElement.innerText = `Score:${score}`;
   }
   for (let i = snakeBody.length - 1; i > 0; i--) {
     //shifting forward the values of the elements in the snake body by one
@@ -56,13 +61,20 @@ const initGame = () => {
   snakeY += velocityY;
 
   //checking if the snakes head is out of the wall,if so setting gameOver to true
-  if (snakeX <= 0 || snakeX > 30 || snakeX <= 0 || snakeY > 30) {
+  if (snakeX <= 0 || snakeX > 30 || snakeY <= 0 || snakeY > 30) {
     gameOver = true;
   }
-``
+  ``;
   for (let i = 0; i < snakeBody.length; i++) {
     //adding a div for each part of snake body
     htmlMarkup += `<div class="head" style= "grid-area: ${snakeBody[i][1]} / ${snakeBody[i][0]}"></div>`;
+    if (
+      i !== 0 &&
+      snakeBody[0][1] === snakeBody[i][1] &&
+      snakeBody[0][0] === snakeBody[i][0]
+    ) {
+      gameOver = true;
+    }
   }
 
   playBoard.innerHTML = htmlMarkup;
