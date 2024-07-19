37ecf1b24bedf336b91b7fad1696e42cee0fd929
akshatsinghania <singhaniaakshat1@gmail.com>
2024-07-19 14:38:10 +0530

    further coded to updating the snake head position based on the current velocity

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const playBoard = document.querySelector(".play-board");
 let foodX, foodY;
 let snakeX = 5,
   snakeY = 10;
+let snakeBody = [];
 let velocityX = 0,
   velocityY = 0;
 const changeFoodPosition = () => {
@@ -26,18 +27,28 @@ const changeDirection = (e) => {
     velocityX = 1;
     velocityY = 0;
   }
-  initGame();
 };
 
 const initGame = () => {
   let htmlMarkup = `<div class="food" style= "grid-area: ${foodY} / ${foodX}"></div>`;
+
+  if (snakeX === foodX && snakeY === foodY) {
+    changeFoodPosition();
+    snakeBody.push([foodX, foodY]); //pushing food position to snake body array
+    console.log(snakeBody);
+  }
+  snakeBody[0] = [snakeX, snakeY]; //setting first element of the snake body to current snake position
   //updating the snake head position based on the current velocity
   snakeX += velocityX;
   snakeY += velocityY;
 
-  htmlMarkup += `<div class="head" style= "grid-area: ${snakeY} / ${snakeX}"></div>`;
+  for (let i = 0; i < snakeBody.length; i++) {
+    //adding a div for each part of snake body
+    htmlMarkup += `<div class="head" style= "grid-area: ${snakeBody[i][1]} / ${snakeBody[i][0]}"></div>`;
+  }
+
   playBoard.innerHTML = htmlMarkup;
 };
 changeFoodPosition();
-initGame();
+setInterval(initGame, 125);
 document.addEventListener("keydown", changeDirection);
